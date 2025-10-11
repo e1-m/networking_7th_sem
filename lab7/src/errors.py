0fa5e99739e15211d172f9639c5a657f278f2bf4
Ivan Mihun <vsk76090@gmail.com>
2025-10-11 13:47:18 +0200

    feat(lab7): setup fastapi app, add exception handling

---
+++ lab7/src/errors.py
@@ -0,0 +1,5 @@
+class CryptographyServiceError(Exception):
+    def __init__(self, message: str = "Service is not available", headers=None):
+        self.message = message
+        self.headers = headers
+        super().__init__(self.message)
---
+++ lab7/src/main.py
@@ -0,0 +1,44 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.errors import (
+    CryptographyServiceError,
+)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def create_exception_handler(status_code, initial_detail):
+    async def exception_handler(_: Request, exc: CryptographyServiceError) -> JSONResponse:
+        content = {"detail": exc.message if exc.message else initial_detail}
+        return JSONResponse(status_code=status_code, content=content, headers=exc.headers)
+
+    return exception_handler
+
+
+exception_handlers = [
+    (CryptographyServiceError, status.HTTP_500_INTERNAL_SERVER_ERROR, "Internal Server Error"),
+]
+
+for exc_class, status_code, message in exception_handlers:
+    app.add_exception_handler(
+        exc_class_or_status_code=exc_class,
+        handler=create_exception_handler(status_code, message)
+    )
