e81e98f96bcfdec535738928410bc8ce0d207c22
Ivan Mihun <vsk76090@gmail.com>
2025-10-18 20:18:15 +0200

    refactor(lab7-9): extract `AsyncDecryptor` interface and restructure RSA decryption logic

---
+++ lab7-9/server/src/decryptor.py
@@ -0,0 +1,28 @@
+import base64
+from abc import ABC, abstractmethod
+
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+class AsyncDecryptor(ABC):
+    @abstractmethod
+    def decrypt(self, cipher_text_b64): ...
+
+
+class RsaDecryptor(AsyncDecryptor):
+    def __init__(self, private_key_pem: bytes) -> None:
+        self.private_key_pem = private_key_pem
+
+    def decrypt(self, cipher_text_b64: str) -> str:
+        private_key = serialization.load_pem_private_key(self.private_key_pem, password=None)
+        cipher_bytes = base64.b64decode(cipher_text_b64)
+        plain_bytes = private_key.decrypt(
+            cipher_bytes,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
+            ),
+        )
+        return plain_bytes.decode('utf-8')
