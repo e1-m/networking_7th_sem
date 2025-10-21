f2f0c40c4a1a256abaefb541aa68b179bac1b4b1
Ivan Mihun <vsk76090@gmail.com>
2025-10-21 14:53:01 +0200

    feat(lab7-9): add `SessionManager` and `ServerApi` for session handling and secure communication

---
+++ lab7-9/client/src/server_api.py
@@ -0,0 +1,38 @@
+import json
+
+import aiohttp
+from pydantic import BaseModel
+
+from src.aes_cryptor import AesCryptor
+from src.session_manager import SessionManager
+
+
+class MessageIn(BaseModel):
+    message: str
+
+
+class MessageOut(BaseModel):
+    message: str
+
+
+class ServerApi:
+    def __init__(self, base_url: str, http_client: aiohttp.ClientSession, session_manager: SessionManager):
+        self.base_url = base_url
+        self.http_client = http_client
+        self.session_manager = session_manager
+
+    async def send_message(self, message: MessageIn) -> MessageOut:
+        session = await self.session_manager.get_session()
+        cryptor = AesCryptor(session.aes_key)
+
+        plaintext_bytes = json.dumps(message.model_dump(mode="json")).encode("utf-8")
+        encrypted_body_b64 = cryptor.encrypt(plaintext_bytes)
+
+        async with self.http_client.post(
+                f"{self.base_url}/echo/",
+                data=encrypted_body_b64,
+                headers={"session-id": session.session_id, "content-type": "application/json"},
+        ) as resp:
+            enc_response = await resp.read()
+            decrypted = cryptor.decrypt(enc_response.decode("utf-8"))
+            return MessageOut(**json.loads(decrypted.decode("utf-8")))
---
+++ lab7-9/client/src/session_manager.py
@@ -0,0 +1,70 @@
+import base64
+from dataclasses import dataclass
+
+import aiohttp
+
+from src.key_generator import AesGenerator
+from src.rsa_encryptor import RsaEncryptor
+
+
+@dataclass
+class Session:
+    session_id: str
+    aes_key: bytes
+
+
+@dataclass
+class _PublicKey:
+    public_key_pem: bytes
+    key_id: str
+
+
+class SessionManager:
+    def __init__(
+            self,
+            base_url: str,
+            http_client: aiohttp.ClientSession,
+            key_generator: AesGenerator,
+
+    ):
+        self.base_url = base_url
+        self.key_generator = key_generator
+        self.http_client = http_client
+
+        self._session: Session | None = None
+
+    async def _fetch_public_key(self) -> _PublicKey:
+        async with self.http_client.get(f"{self.base_url}/crypto-keys/public-key") as resp:
+            data = await resp.json()
+
+            return _PublicKey(
+                public_key_pem=data["public_key"].encode("ascii"),
+                key_id=data["key_id"],
+            )
+
+    async def _initiate_new_session(self) -> Session:
+        public_key = await self._fetch_public_key()
+
+        aes_key = self.key_generator.generate_secret_key()
+        aes_key_b64 = base64.b64encode(aes_key).decode("ascii")
+
+        encryptor = RsaEncryptor(public_key.public_key_pem)
+        encrypted_aes_key_b64 = encryptor.encrypt(aes_key_b64)
+
+        async with self.http_client.post(
+                f"{self.base_url}/sessions",
+                json={"aes_key_b64_encrypted": encrypted_aes_key_b64},
+                headers={"x-rsa-id": f"{public_key.key_id}"},
+        ) as resp:
+            session_data = await resp.json()
+            return Session(
+                session_id=session_data["session_id"],
+                aes_key=aes_key,
+            )
+
+    async def get_session(self) -> Session:
+        if self._session is not None:
+            return self._session
+
+        self._session = await self._initiate_new_session()
+        return self._session
